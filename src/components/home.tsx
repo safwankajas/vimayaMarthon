ccac68fe9078fada3654c19a46b164607f6c1a54
safwankajas <safwankajas02@gmail.com>
2025-07-26 12:19:56 +0530

    home-demo

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -28,7 +28,7 @@ export const HomePage = () => {
           left: 0,
           right: 0,
           bottom: 0,
-          backgroundImage: "url('/Home1.jpg')",
+          backgroundImage: "url('/Home2.png')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           // transform: `translateY(${offsetY * 0.3}px)`,
