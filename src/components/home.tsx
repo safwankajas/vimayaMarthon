9efc1c35c41791f8530d814ceb9c7b94290e45d5
safwankajas <safwankajas02@gmail.com>
2025-08-02 19:42:24 +0530

    add-brightness

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -44,7 +44,7 @@ export const HomePage = () => {
           right: 0,
           bottom: 0,
 
-          background: "rgba(0, 0, 0, 0.3)",
+          background: "rgba(0, 0, 0, 0.2)",
           zIndex: 1,
         }}
       >
