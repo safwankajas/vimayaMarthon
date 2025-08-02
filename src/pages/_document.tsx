32d54f9a00e06236438eaaa326792c144d99c7d8
safwankajas <safwankajas02@gmail.com>
2025-08-02 15:53:42 +0530

    demo-document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,61 +1,13 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="Join the Vismay Run 2025 in Kochi on October 5. Celebrate endurance, fitness, and fun in our 5K marathon event. Register now!"
-        />
-        <meta
-          name="keywords"
-          content="Vismay Run, Kochi Marathon, 5K Run, Sports Event, October 2025, Vismay 2025, Kerala Event"
-        />
-        <meta name="author" content="Vismay Team" />
-
-        <link rel="canonical" href="https://vismayrun2025.in" />
-        <meta name="robots" content="index, follow" />
-
-        {/* Open Graph for social preview */}
-        <meta property="og:title" content="Vismay Run 2025" />
-        <meta
-          property="og:description"
-          content="Join the Vismay Run 2025 in Kochi on October 5. Be part of the most exciting 5K event!"
-        />
-        <meta
-          property="og:image"
-          content="https://vismayrun2025.in/home2.webp"
-        />
-        <meta property="og:url" content="https://vismayrun2025.in" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Vismay Run 2025" />
-        <meta
-          name="twitter:description"
-          content="Join the 5K Vismay Run in Kochi this October."
-        />
-
-        {/* Theme color */}
-        <meta name="theme-color" content="#620b38" />
-
-        {/* Font Optimization */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Red+Rose:wght@300..700&display=swap"
           rel="stylesheet"
         />
-
-        {/* Favicon */}
-        <link rel="icon" href="/VISMAY-RUN-LOGO.ico" />
       </Head>
       <body className="antialiased">
         <Main />
